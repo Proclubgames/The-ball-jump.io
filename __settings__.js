d63537baf556b8f057973582281b25c23475da49
Proclubgames <manideep8582@gmail.com>
2025-07-14 12:28:07 +0530

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2280202.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 240439379, 240439380, 240439378, 240439553, 240439558 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/240439397/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/240439401/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/240439392/1/ammo.js', 'preload' : true},
+];
